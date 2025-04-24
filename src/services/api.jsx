807b5e398d11dd2d02504f270176cc5ca7ce8249
bg987 <bhavyagodhaniya2004@gmail.com>
2025-04-24 23:01:50 +0530

    url fix

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,7 +3,7 @@ import { useEffect, } from "react";
 import { useNavigate } from "react-router-dom";
 
 const API = axios.create({
-    baseURL: "http://192.168.121.47:5000", // Backend URL
+    baseURL: "https://sellease-backend.onrender.com", // Backend URL
     withCredentials: true
 });
 //"http://192.168.121.47:5000"
